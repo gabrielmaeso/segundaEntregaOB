0d890e2eaaf402ccf71441e8fca5538ec6134bb3
gabriel <gabrielmaeso@hotmail.com>
2023-01-01 18:41:36 -0300

    segunda entrega ob

---
+++ index.js
@@ -0,0 +1,14 @@
+let nuevaLista= [nombre="Gabriel Emiliano Maeso",
+edad= 27,
+eresDesarrollador= true,
+nacimiento= new Date ("07/04/1995"), 
+libroFavorito={
+    titulo: "Habitos atomicos",
+    autor: "James Clear",
+    fecha: "16 de octubre de 2018",
+    url: "https://listado.mercadolibre.com.ar/habitos-atomicos-james-clear"
+}]
+ 
+console.log(nuevaLista);
+
+
